a057ba1a326f9e351514bb57f60ed6e5a22300df
cloudgamingrage <136248244+cloudgamingrage@users.noreply.github.com>
2024-09-08 18:40:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+curl -Ls https://raw.githubusercontent.com/cloudgamingrage/Pterodactyl-VPS-Egg/main/install.sh -o install.sh
+curl -Ls https://raw.githubusercontent.com/cloudgamingrage/Pterodactyl-VPS-Egg/main/run.sh -o run.sh
+
 # Copy run.sh to /home/container
 cp /run.sh "$HOME/run.sh"
 
